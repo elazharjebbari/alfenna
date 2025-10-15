d55c440081281d81673fd7bcd60406189524a5bd
elazharjebbari <elazhar.jebbari@gmail.com>
2025-10-15 09:36:27 +0200

    Traduction OK with backend

--- apps/content/migrations/0007_alter_lecturetranslation_master_and_more.py
+++ apps/content/migrations/0007_alter_lecturetranslation_master_and_more.py
@@ -1,6 +1,47 @@
 import django.db.models.deletion
+from django.db import migrations, models
+from django.db.migrations.operations.base import Operation
+
 import parler.fields
-from django.db import migrations
+import parler.models
+
+
+class AlterModelBases(Operation):
+    """
+    Ensure historical Section/Lecture models use TranslatableModel so parler
+    can attach translation metadata during migrations.
+    """
+
+    reduces_to_sql = True
+    reversible = True
+
+    def __init__(self, name, new_bases, old_bases):
+        self.name = name
+        self.new_bases = new_bases
+        self.old_bases = old_bases
+
+    def state_forwards(self, app_label, state):
+        model_state = state.models[app_label, self.name.lower()]
+        cloned = model_state.clone()
+        cloned.bases = self.new_bases
+        state.models[app_label, self.name.lower()] = cloned
+        state.reload_model(app_label, self.name.lower(), delay=True)
+
+    def state_backwards(self, app_label, state):
+        model_state = state.models[app_label, self.name.lower()]
+        cloned = model_state.clone()
+        cloned.bases = self.old_bases
+        state.models[app_label, self.name.lower()] = cloned
+        state.reload_model(app_label, self.name.lower(), delay=True)
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        pass
+
+    def describe(self):
+        return f"Alter {self.name} model bases to parler.TranslatableModel"
 
 
 class Migration(migrations.Migration):
@@ -10,6 +51,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        AlterModelBases(
+            name="Section",
+            new_bases=(parler.models.TranslatableModel,),
+            old_bases=(models.Model,),
+        ),
+        AlterModelBases(
+            name="Lecture",
+            new_bases=(parler.models.TranslatableModel,),
+            old_bases=(models.Model,),
+        ),
         migrations.AlterField(
             model_name="lecturetranslation",
             name="master",
